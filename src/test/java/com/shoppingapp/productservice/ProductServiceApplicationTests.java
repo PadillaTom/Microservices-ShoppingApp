6409bb9763983d51c688d60fb607d94c59888726
Tomas Padilla <padillatomasagustin@gmail.com>
2022-05-12 08:25:55 +0200

    Products - Testcontainers, CreateProdutTest()

--- src/test/java/com/shoppingapp/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/shoppingapp/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,69 @@
 package com.shoppingapp.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shoppingapp.productservice.model.request.ProductRequest;
+import com.shoppingapp.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	MappingJackson2HttpMessageConverter jacksonMessageConverter = new MappingJackson2HttpMessageConverter();
+	ObjectMapper objectMapper = jacksonMessageConverter.getObjectMapper();
+
+	@Autowired
+	private ProductRepository productRepository;
+
+
+	//	Set property for database URI (on runtime)
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+//		POST 201
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(getProductRequest())))
+				.andExpect(status().isCreated());
+//		FindAll()
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+	}
+
+//	=== Product Request ===
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone 13")
+				.description("The best in 2022")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
